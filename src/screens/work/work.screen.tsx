013322c8487e6b66dd2e87d19d27acc12817f7df
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-06-27 18:36:24 +0200

    Update work.screen.tsx

--- src/screens/work/work.screen.tsx
+++ src/screens/work/work.screen.tsx
@@ -2,16 +2,16 @@ import Code from '../../assets/images/work/code2.png';
 
 const Work = () => {
   return (
-    <div data-name='work' className='w-full h-screen text-gray-300 bg-[#0a192f]'>
+    <div data-name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8 pt-16 w-full flex justify-center items-center flex-col mb-4 '>
+        <div className='pb-8 pt-10 w-full flex justify-center items-center flex-col mb-4 mt-4'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500'>
             Work
           </p>
           <p className='py-6 text-2xl'>Check out some of my most recent work</p>
         </div>
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          <div className='aspect-w-1 aspect-h-1' />
+          <div className='hidden md:block' /> {/* Spacer element */}
           <div className='aspect-w-1 aspect-h-1'>
             <div
               style={{ backgroundImage: `url(${Code})` }}
@@ -22,7 +22,7 @@ const Work = () => {
                   DJ FLAVA WEB APPLICATION
                 </span>
                 <p className='text-center'>
-                  A web application built with REACT, TYPESCRIPT, NODE.JS, TAILWINDCSS, JAVASCRIPT.
+                  A web application built with REACT, TYPSCRIPT, NODE.JS, TAILWINDCSS, JAVASCRIPT.
                 </p>
                 <div className='pt-8 text-center'>
                   <a href='https://www.djflava.org/home' target='_blank' rel='noopener noreferrer'>
@@ -66,7 +66,7 @@ const Work = () => {
                   TINDOG WEB APPLICATION
                 </span>
                 <p className='text-center'>
-                  A web application built with JAVASCRIPT, HTML, CSS, BOOTSTRAP.
+                  A web application built with JAVASCRIPT, HTML, CSS, BOOTSRAP.
                 </p>
                 <div className='pt-8 text-center'>
                   <a href='https://bootstrap-tindog-website-5zsc3md03-juandresabbat92-gmailcom.vercel.app/' target='_blank' rel='noopener noreferrer'>
@@ -105,6 +105,5 @@ const Work = () => {
     </div>
   );
 };
-
 export default Work;
 
