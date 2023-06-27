dda0c2417a5cfce017175b8b73de40a390077328
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-06-27 15:15:50 +0200

    Update about.screen.tsx

--- src/screens/about/about.screen.tsx
+++ src/screens/about/about.screen.tsx
@@ -3,18 +3,13 @@ const About = () => {
     <div data-name="about" id="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-5/6 md:w-3/6">
-          <div className="max-w-[400px] w-full grid sm:grid-cols-2 gap-8 mb-4">
+          <div className="max-w-[400px] w-full grid sm:grid-cols-1 gap-8 mb-4">
             <div className="text-center pb-8 pl-4">
               <p className="text-4xl font-bold inline border-b-4 border-cyan-500">About</p>
             </div>
-            <div></div>
-          </div>
-          <div className="max-w-[400px] w-full grid sm:grid-cols-2 gap-8 px-4">
-            <div className="text-center sm:text-right md:text-center lg:text-right">
-              <p className="text-lg sm:text-3xl">Hi. I'm Juandre Sabbat, nice to meet you.</p>
-            </div>
-            <div className="text-center sm:text-left md:text-center lg:text-left">
-              <p className="text-base sm:text-3xl md:text-lg lg:text-xl">
+            <div className="text-center">
+              <p className="text-lg sm:text-3xl">
+                Hi. I'm Juandre Sabbat, nice to meet you.{" "}
                 With a strong background in full-stack development, I excel in designing and implementing elegant solutions that deliver optimal user experiences and drive business growth.
               </p>
             </div>
