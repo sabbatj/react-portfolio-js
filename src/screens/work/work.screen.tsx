96fdc9ac5096411ea153bee2bb0470cf78386738
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-06-27 18:16:59 +0200

    Update work.screen.tsx

--- src/screens/work/work.screen.tsx
+++ src/screens/work/work.screen.tsx
@@ -1,49 +1,104 @@
-const Skills = () => {
+import Code from '../../assets/images/work/code2.png';
+
+const Work = () => {
   return (
-    <div data-name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div data-name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='w-full flex justify-center items-center flex-col mb-7 mt-12'>
-          <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-center '>Skills</p>
-          <p className='py-4 text-2xl'>I am passionate about continuously expanding my skill set and staying up-to-date with the latest technologies and trends. Here are some of the technologies I have experience with:</p>
-        </div> 
-      </div>
-      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <p className='my-4'>HTML</p>
+        <div className='pb-8 w-full flex justify-center items-center flex-col mb-4 mt-4'>
+          <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500'>
+            Work
+          </p>
+          <p className='py-6 text-2xl'>Check out some of my most recent work</p>
+        </div>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          <div className='hidden md:block' /> {/* Spacer element */}
+          <div className='aspect-w-1 aspect-h-1'>
+            <div
+              style={{ backgroundImage: `url(${Code})` }}
+              className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            >
+              <div className='flex justify-center items-center flex-col'>
+                <span className='text-lg font-bold text-white tracking-wider'>
+                  DJ FLAVA WEB APPLICATION
+                </span>
+                <p className='text-center'>
+                  A web application built with REACT, TYPSCRIPT, NODE.JS, TAILWINDCSS, JAVASCRIPT.
+                </p>
+                <div className='pt-8 text-center'>
+                  <a href='https://www.djflava.org/home' target='_blank' rel='noopener noreferrer'>
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                      Live
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <p className='my-4'>CSS</p>
+          <div className='aspect-w-1 aspect-h-1'>
+            <div
+              style={{ backgroundImage: `url(${Code})` }}
+              className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            >
+              <div className='flex justify-center items-center flex-col'>
+                <span className='text-lg font-bold text-white tracking-wider'>
+                  PORTFOLIO WEB APPLICATION
+                </span>
+                <p className='text-center'>
+                  A web application built with JAVASCRIPT, HTML, CSS.
+                </p>
+                <div className='pt-8 text-center'>
+                  <a href='https://juandresabbatportfolio.vercel.app/' target='_blank' rel='noopener noreferrer'>
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                      Live
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <p className='my-4'>JAVASCRIPT</p>
+          <div className='aspect-w-1 aspect-h-1'>
+            <div
+              style={{ backgroundImage: `url(${Code})` }}
+              className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            >
+              <div className='flex justify-center items-center flex-col'>
+                <span className='text-lg font-bold text-white tracking-wider'>
+                  TINDOG WEB APPLICATION
+                </span>
+                <p className='text-center'>
+                  A web application built with JAVASCRIPT, HTML, CSS, BOOTSRAP.
+                </p>
+                <div className='pt-8 text-center'>
+                  <a href='https://bootstrap-tindog-website-5zsc3md03-juandresabbat92-gmailcom.vercel.app/' target='_blank' rel='noopener noreferrer'>
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                      Live
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <p className='my-4'>REACT</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <p className='my-4'>GITHUB</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <p className='my-4'>NODE JS</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <p className='my-4'>MONGO DB</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <p className='my-4'>GITLAB</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <p className='my-4'>RUBY ON RAILS</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <p className='my-4'>POSTGRE-SQLl</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <p className='my-4'>TYPESCRIPT</p>
-          </div>
-          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-            <p className='my-4'>CSHARP</p>
+          <div className='aspect-w-1 aspect-h-1'>
+            <div
+              style={{ backgroundImage: `url(${Code})` }}
+              className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+            >
+              <div className='flex justify-center items-center flex-col'>
+                <span className='text-lg font-bold text-white tracking-wider'>
+                  NGOS WEB APPLICATION
+                </span>
+                <p className='text-center'>
+                  A web application built with HTML, CSS.
+                </p>
+                <div className='pt-8 text-center'>
+                  <a href='https://ngos.co.za/' target='_blank' rel='noopener noreferrer'>
+                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                      Live
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -51,5 +106,4 @@ const Skills = () => {
   );
 };
 
-export default Skills;
-
+export default Work;
