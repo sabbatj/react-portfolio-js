5bf5ef8c40170b815e7f22eddf8a6c9e2c4cd166
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-06-27 14:06:44 +0200

    Update about.screen.tsx

--- src/screens/about/about.screen.tsx
+++ src/screens/about/about.screen.tsx
@@ -20,8 +20,7 @@ const About = () => {
           <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
             <div className="sm:text-right text-4xl font-bold">
               <p>
-                Hi. I'm Juandre Sabbat, nice to meet you. Please take a
-                look around.
+                Hi. I'm Juandre Sabbat, nice to meet you.
               </p>
             </div>
             <div>
