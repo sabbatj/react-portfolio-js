4e6fc462393601693805d6dea533c8d64cdfee08
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-06-27 14:41:35 +0200

    Update about.screen.tsx

--- src/screens/about/about.screen.tsx
+++ src/screens/about/about.screen.tsx
@@ -3,9 +3,9 @@ const About = () => {
     <div data-name="about" id="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-4/6">
-          <div className="max-w-[400px] w-full grid grid-cols-2 gap-8 mb-4">
-            <div className="sm:text-right pb-8 pl-4">
-              <p className="text-4xl font-bold inline border-b-4 border-cyan-500 text-center">About</p>
+          <div className="max-w-[400px] w-full grid sm:grid-cols-2 gap-8 mb-4">
+            <div className="sm:text-center pb-8 pl-4">
+              <p className="text-4xl font-bold inline border-b-4 border-cyan-500">About</p>
             </div>
             <div></div>
           </div>
@@ -14,7 +14,7 @@ const About = () => {
               <p className="text-center">Hi. I'm Juandre Sabbat, nice to meet you.</p>
             </div>
             <div className="flex justify-center items-center h-full">
-              <p className="text-center">
+              <p className="text-center sm:text-left"> {/* Added sm:text-left class */}
                 With a strong background in full-stack development, I excel in designing and implementing elegant solutions that deliver optimal user experiences and drive business growth.
               </p>
             </div>
@@ -26,5 +26,4 @@ const About = () => {
 };
 
 export default About;
-
 
