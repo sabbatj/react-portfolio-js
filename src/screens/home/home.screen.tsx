19b737d3ec65c96991e77235e4614fd91db19545
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-06-27 13:23:58 +0200

    Update home.screen.tsx

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -14,7 +14,7 @@ const Home = () => {
 
   const imageVariants = {
     hidden: { opacity: 0, y: 100 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.5, delay: 0.4 } },
+    visible: { opacity: 1, y: 5, transition: { duration: 0.5, delay: 0.4 } },
   };
 
   return (
