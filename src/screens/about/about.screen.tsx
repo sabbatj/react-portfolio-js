0b2ea15ae9a74566048fec34ea17a375729ad80e
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-06-27 14:53:09 +0200

    Update about.screen.tsx

--- src/screens/about/about.screen.tsx
+++ src/screens/about/about.screen.tsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <div data-name="about" id="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
       <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-5/6 md:w-4/6"> {/* Updated w-4/6 to w-5/6 for desktop view */}
+        <div className="py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-5/6 md:w-3/6"> {/* Updated w-4/6 to w-5/6 for desktop view */}
           <div className="max-w-[400px] w-full grid sm:grid-cols-2 gap-8 mb-4">
             <div className="text-center pb-8 pl-4">
               <p className="text-4xl font-bold inline border-b-4 border-cyan-500">About</p>
