feaefb0fab7864167c15567bb4410bd94b9bb5a5
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-06-27 13:16:05 +0200

    Update home.screen.tsx

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -30,7 +30,7 @@ const Home = () => {
             I'm a Full Stack Developer
           </h2>
           <motion.p className="text-gray-500 py-2 px-2 max-w-md" variants={textVariants}>
-            I have 2 years of experience in mobile and web development. Equipped with a diverse range of programming languages, frameworks, and tools, I am adept at seamlessly bridging the gap between user experience and server functionality.
+            I have experience in mobile and web development. Equipped with a diverse range of programming languages, frameworks, and tools, I am adept at seamlessly bridging the gap between user experience and server functionality.
           </motion.p>
         </motion.div>
         <motion.div variants={imageVariants}>
