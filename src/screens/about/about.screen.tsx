59f2f717c0e218a0d1b99ce35eec5df154d10c5a
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-06-27 18:24:53 +0200

    Update about.screen.tsx

--- src/screens/about/about.screen.tsx
+++ src/screens/about/about.screen.tsx
@@ -3,7 +3,7 @@ const About = () => {
     <div data-name="about" id="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-5/6 md:w-3/6 transition-all duration-500 hover:scale-95">
-          <div className="max-w-[400px] w-full grid sm:grid-cols-1 gap-8 mb-4">
+          <div className="max-w-[400px] w-full grid sm:grid-cols-1 gap-8 mb-4 mt-4">
             <div className="text-center pb-8 pl-4">
               <p className="text-4xl font-bold inline border-b-4 border-cyan-500">About</p>
             </div>
