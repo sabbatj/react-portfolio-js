d2ec5a43ac354da7e29d5268c8f888e4e7eb3336
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-06-27 14:51:08 +0200

    Update about.screen.tsx

--- src/screens/about/about.screen.tsx
+++ src/screens/about/about.screen.tsx
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <div data-name="about" id="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
       <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-4/6">
+        <div className="py-16 rounded-md bg-cyan-800 flex flex-col justify-center items-center w-5/6 md:w-4/6"> {/* Updated w-4/6 to w-5/6 for desktop view */}
           <div className="max-w-[400px] w-full grid sm:grid-cols-2 gap-8 mb-4">
             <div className="text-center pb-8 pl-4">
               <p className="text-4xl font-bold inline border-b-4 border-cyan-500">About</p>
@@ -11,10 +11,10 @@ const About = () => {
           </div>
           <div className="max-w-[400px] w-full grid sm:grid-cols-2 gap-8 px-4">
             <div className="sm:text-right text-4xl font-bold flex justify-center items-center h-full">
-              <p className="text-center text-lg sm:text-2xl">Hi. I'm Juandre Sabbat, nice to meet you.</p> {/* Updated text-lg and sm:text-2xl classes */}
+              <p className="text-center text-lg sm:text-2xl">Hi. I'm Juandre Sabbat, nice to meet you.</p>
             </div>
             <div className="flex justify-center items-center h-full">
-              <p className="text-center sm:text-left text-base sm:text-lg"> {/* Updated text-base and sm:text-lg classes */}
+              <p className="text-center sm:text-left text-base sm:text-lg">
                 With a strong background in full-stack development, I excel in designing and implementing elegant solutions that deliver optimal user experiences and drive business growth.
               </p>
             </div>
