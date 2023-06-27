620f39c2505bba033669f94a96e9ed31d84d055c
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-06-27 17:58:24 +0200

    Update work.screen.tsx

--- src/screens/work/work.screen.tsx
+++ src/screens/work/work.screen.tsx
@@ -4,7 +4,7 @@ const Work = () => {
   return (
     <div data-name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8 w-full flex justify-center items-center flex-col mb-8 md:mb-0'>
+        <div className='pb-8 w-full flex justify-center items-center flex-col mb-8 mb-04'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500'>
             Work
           </p>
