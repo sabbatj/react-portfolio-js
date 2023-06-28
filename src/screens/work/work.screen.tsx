6c0a900479f200889447898ccb9c3f32b68c9783
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-06-28 09:05:26 +0200

    Update work.screen.tsx

--- src/screens/work/work.screen.tsx
+++ src/screens/work/work.screen.tsx
@@ -1,22 +1,21 @@
-import Code from '../../assets/images/work/code2.png';
+              import Code from '../../assets/images/work/code2.png';
 
 const Work = () => {
   return (
     <div data-name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-        <div className='pb-8 pt-10 w-full flex justify-center items-center flex-col mb-4 mt-4'>
+        <div className='pb-8 w-full flex justify-center items-center flex-col mb-4 mt-4'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500'>
             Work
           </p>
           <p className='py-6 text-2xl'>Check out some of my most recent work</p>
         </div>
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          <div className='hidden md:block' /> {/* Spacer element */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
           <div className='aspect-w-1 aspect-h-1'>
             <div
               style={{ backgroundImage: `url(${Code})` }}
-              className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-            >
+              className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div p-4'
+              >
               <div className='flex justify-center items-center flex-col'>
                 <span className='text-lg font-bold text-white tracking-wider'>
                   DJ FLAVA WEB APPLICATION
