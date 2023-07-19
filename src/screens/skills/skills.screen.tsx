5cab689604021c472abafa0cf7524233b590d29c
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-07-19 08:38:51 +0200

    Update skills.screen.tsx

--- src/screens/skills/skills.screen.tsx
+++ src/screens/skills/skills.screen.tsx
@@ -8,7 +8,7 @@ const Skills = () => {
           </div> 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <p className='my-4'>HTML</p>
+                  <p className='my-4'>HTML CSS</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <p className='my-4'>TAILWINDCSS</p>
