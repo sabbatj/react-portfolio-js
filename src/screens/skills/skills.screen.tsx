740e3bc6c33ec5ea78c2ba3ecde0dc4044d65d43
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-07-21 10:51:13 +0200

    Update skills.screen.tsx

--- src/screens/skills/skills.screen.tsx
+++ src/screens/skills/skills.screen.tsx
@@ -4,7 +4,7 @@ const Skills = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div className=' w-full flex justify-center items-center flex-col mb-7 mt-12'>
               <p className='text-4xl font-bold inline border-b-4 border-cyan-500 text-center '>Skills</p>
-              <p className='py-4 text-2xl'>Passionately dedicated to continuous skill expansion, staying updated with cutting-edge technologies and trends. Proficient in a diverse range of technologies for delivering innovative solutions. Here are some of the technologies I have experience with:</p>
+              <p className='py-4 text-2xl'>Proficient in a diverse range of technologies for delivering innovative solutions. Here are some of the technologies I have experience with:</p>
           </div> 
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
