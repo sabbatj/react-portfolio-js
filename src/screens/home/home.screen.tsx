4ee56f95f7d83808519e25caab8caa249c30ddcf
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-07-21 10:59:32 +0200

    Update home.screen.tsx

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -33,7 +33,7 @@ const Home = () => {
             Versatile full-stack developer, combining technical prowess and creativity to deliver impactful solutions and elevate user experiences.
           </motion.p>
         </div>
-        <motion.div variants={imageVariants} className="mt-8 md:mt-0"> {/* Adjusted margin-top value */}
+        <motion.div variants={imageVariants} className="mt-10 md:mt-0"> {/* Adjusted margin-top value */}
           <img
             src={me}
             alt="my profile"
