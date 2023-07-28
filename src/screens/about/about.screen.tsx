a0ab3c14753e232d2f7e97f682ebdf10e63e2b57
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-07-28 21:58:03 +0200

    Update about.screen.tsx

--- src/screens/about/about.screen.tsx
+++ src/screens/about/about.screen.tsx
@@ -5,11 +5,11 @@ const About = () => {
         <div className="py-10 pt-[-10px] rounded-md bg-cyan-800 flex flex-col justify-center items-center w-5/6 md:w-3/6 transition-all duration-500 hover:scale-95">
           <div className="max-w-[400px] pt-10 pb-10 px-4 w-full grid sm:grid-cols-1 gap-8 mb-4">
             <div className="text-center pb-8">
-              <p className="text-4xl font-bold inline border-b-4 border-cyan-500">About</p>
+              <p className="text-4xl font-bold inline border-b-4 border-cyan-500">ABOUT</p>
             </div>
             <div className="text-center">
               <p className="text-lg sm:text-3xl">
-                Experienced full-stack developer with a passion for exceptional user experiences and effective solutions. Notable track record in delivering impactful projects, driven by a user-centric vision. Thrives in teamwork and constantly enhances skills through continuous learning.
+                EXPERIENCED FULL-STACK DEVELOPER WITH A PASSION FOR EXCEPTIONAL USER EXPERIENCES AND EFFECTIVE SOLUTIONS. NOTABLE TRACK RECORD IN DELIVERING IMPACTFUL PROJECTS, DRIVEN BY A USER-CENTRIC VISION. THRIVES IN TEAMWORK AND CONSTANTLY ENHANCES SKILLS THROUGH CONTINUOUS LEARNING.
               </p>
             </div>
           </div>
