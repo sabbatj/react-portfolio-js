7aebf458f3204d7067a6f1f5c8cfa3dfe83ffc15
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-07-28 11:53:48 +0200

    Update home.screen.tsx

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -29,7 +29,7 @@ const Home = () => {
           <motion.h2 className="text-4xl pt-20 pb-5 sm:text-7xl font-bold text-white" variants={textVariants}>
             A FULL STACK DEVELOPER
           </motion.h2>
-          <motion.p className="text-gray-500 py-5 px-5 max-w-md mx-auto" variants={textVariants}>
+          <motion.p className="text-gray-500 py-5 px-5 max-w-md" variants={textVariants}>
           Versatile full-stack developer, combining technical prowess and creativity to deliver impactful solutions and elevate user experiences.
           </motion.p>
         </div>
