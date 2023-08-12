9dd83182d458c8f5b80cd36b63b05f2d03983f66
juandre sabbat <127730982+sabbatj@users.noreply.github.com>
2023-08-12 23:03:21 +0200

    Update home.screen.tsx

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -27,7 +27,6 @@ const Home = () => {
             INNOVATIVE SOFTWARE DEVELOPER
           </motion.h2>
           <motion.p className="text-gray-500 py-5 px-5 max-w-md" variants={textVariants}>
-           VERSATILE SOFTWARE DEVELOPER, COMBINING TECHNICAL PROWESS AND CREATIVITY TO DELIVER IMPACTFUL SOLUTIONS AND ELEVATE USER EXPERIENCES. 
           </motion.p>
         </div>
         <motion.div variants={imageVariants} className="mt-20 md:mt-0">
